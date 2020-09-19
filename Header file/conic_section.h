7984c11679850ef7ef1bad4e99600e5df3a91d0d
Dr2546 <62285718+Dr2546@users.noreply.github.com>
2020-09-19 17:13:44 +0700

    Add files via upload

---
+++ Header file/conic_section.h
@@ -0,0 +1,9 @@
+double line(double x, double m, double c);
+void linecircle(double h, double k, double m, double c, double r);
+void circleintersect(double h1, double k1, double h2, double k2, double r1, double r2);
+void lineline(double m1, double c1, double m2, double c2);
+void lineconic(double a, double b, double c, double d1, double e, double m1, double c1);
+void cir_conic(double a, double b, double c, double d1, double e, double h, double k, double r);
+void parapara(double a1, double b1, double c1, double d1, double e1, double a2, double b2, double c2, double d2, double e2);
+void paraconic(double a1, double b1, double c1, double d1, double e1, double a2, double b2, double c2, double d2, double e2);
+void ellipse_hyperbola(double a1, double b1, double c1, double d1, double e1, double a2, double b2, double c2, double d2, double e2);
